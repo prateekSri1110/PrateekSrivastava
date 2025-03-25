98f8fbf70396909861c7b6db5d456df9fc41c771
PrateekSri1110 <prateek.kumar.srivastava.1110@gmail.com>
2025-03-25 13:23:37 +0530

    up

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import About from './component/about.jsx'
 import Qualifications from './component/qualifications.jsx'
 import Skills from './component/skills.jsx'
 import Projects from './component/projects.jsx'
-// import Footer from './component/Footer.jsx'
+import Footer from './component/Footer.jsx'
 
 function App() {
 
