9055c0836f7cd33a9f7fd655983ba829a5d5876d
PrateekSri1110 <prateek.kumar.srivastava.1110@gmail.com>
2025-07-30 14:49:13 +0530

    skills, CV update and project add

--- src/component/projects.jsx
+++ src/component/projects.jsx
@@ -1,95 +1,142 @@
 export default function Projects() {
-    return (
-        <header id="project">
-            <br /><br /><hr />
-            <section className="container project p-4" id="project">
-
-                <h3 className="text-center">PROJECTS</h3>
-                <br />
-                <div className="image-container">
-                    <div className="row d-flex">
-                        <div className="col-6">
-                            <iframe src="https://cugpacalculator.netlify.app/" style={{ border: "1px solid black" }} width="90%" height="300px" />
-                        </div>
-                        <div className="col-6">
-                            <h3>GPA Calculator</h3>
-                            <p> SPA GPA Calculator
-                                <li>
-                                    Add as many as subjects you want.
-                                </li>
-                                <li>
-                                    Grade Table
-                                </li>
-                                <li>
-                                    seconds of game to calculate semester GPA.
-                                </li>
-                                <a href="https://cugpacalculator.netlify.app/">Link</a>
-                            </p>
-                        </div>
-                    </div>
-                    <hr />
-                    <div className="row d-flex">
-                        <div className="col-6">
-                            <iframe src="https://shayarana.netlify.app/" style={{ border: "1px solid black" }} width="90%" height="300px" />
-                        </div>
-                        <div className="col-6">
-                            <h3>Shayarana</h3>
-                            <p> MERN Stack Project.
-                                <li>
-                                    Poetry and shayari reading and contribution platform.
-                                </li>
-                                <li>
-                                    API building (deployed on netlify).
-                                </li>
-                                <li>
-                                    Upload, Fetch and delete your posts. Authentication and Profiling.
-                                </li>
-                                <a href="https://shayarana.netlify.app/">Link</a>
-                            </p>
-                        </div>
-                    </div>
-                    <hr />
-                    <div className="row d-flex">
-                        <div className="col-6">
-                            <iframe src="https://greenclassifier.streamlit.app/" style={{ border: "1px solid black" }} width="90%" height="300px" />
-                        </div>
-                        <div className="col-6">
-                            <h3>Green Classifier Model</h3>
-                            <p> Vegetable / Fruit Classification model.
-                                <li>
-                                    Developed a Deep Learning model using TensorFlow and Keras to classify images
-                                    of fruits/vegetables.
-                                </li>
-                                <li>
-                                    Deployed on Streamlit with an intuitive UI, achieving 90% classification accuracy.
-                                </li>
-                                <a href="https://greenclassifier.streamlit.app/">Link</a>
-                            </p>
-                        </div>
-                    </div>
-                    <hr />
-                    <div className="row d-flex">
-                        <div className="col-6">
-                            <iframe src="https://prateeksri1110.github.io/Teahub/" style={{ border: "1px solid black" }} width="90%" height="300px" />
-                        </div>
-                        <div className="col-6">
-                            <h3>TeaHub</h3>
-                            <p> Tea ordering Web Portal
-                                <li>
-                                    Developed this web application with the help of tailwind CSS.
-                                </li>
-                                <li>
-                                    Deployed on Github Pages.
-                                </li>
-                                <a href='https://prateeksri1110.github.io/Teahub/'>Link</a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <br /><br />
-                <h4 className='text-center'>Check Out my <a href="https://github.com/prateekSri1110">Github</a> for more Porjects.</h4>
-                <br /><hr />
-            </section>
-        </header>
-    );
+  return (
+    <header id="project">
+      <br />
+      <br />
+      <hr />
+      <section className="container project p-4" id="project">
+        <h3 className="text-center">PROJECTS</h3>
+        <br />
+        <div className="image-container">
+          <div className="row d-flex">
+            <div className="col-6">
+              <iframe
+                src="https://github.com/prateekSri1110/ProgramMate"
+                style={{ border: "1px solid black" }}
+                width="90%"
+                height="300px"
+              />
+            </div>
+            <div className="col-6">
+              <h3>ProgramMate</h3>
+              <p>
+                {" "}
+                ProgramMate - Programming Application
+                <li>User Login using Google sign-in method.</li>
+                <li>Random Question generate and code submission.</li>
+                <li>Admin Authentication and Verification.</li>
+                <li>Admin add/remove and submission view rights.</li>
+                <a href="https://github.com/prateekSri1110/ProgramMate">Link</a>
+              </p>
+            </div>
+          </div>
+          <hr />
+          <div className="row d-flex">
+            <div className="col-6">
+              <iframe
+                src="https://cugpacalculator.netlify.app/"
+                style={{ border: "1px solid black" }}
+                width="90%"
+                height="300px"
+              />
+            </div>
+            <div className="col-6">
+              <h3>GPA Calculator</h3>
+              <p>
+                {" "}
+                SPA GPA Calculator
+                <li>Add as many as subjects you want.</li>
+                <li>Grade Table</li>
+                <li>seconds of game to calculate semester GPA.</li>
+                <a href="https://cugpacalculator.netlify.app/">Link</a>
+              </p>
+            </div>
+          </div>
+          <hr />
+          <div className="row d-flex">
+            <div className="col-6">
+              <iframe
+                src="https://shayarana.netlify.app/"
+                style={{ border: "1px solid black" }}
+                width="90%"
+                height="300px"
+              />
+            </div>
+            <div className="col-6">
+              <h3>Shayarana</h3>
+              <p>
+                {" "}
+                MERN Stack Project.
+                <li>Poetry and shayari reading and contribution platform.</li>
+                <li>API building (deployed on netlify).</li>
+                <li>
+                  Upload, Fetch and delete your posts. Authentication and
+                  Profiling.
+                </li>
+                <a href="https://shayarana.netlify.app/">Link</a>
+              </p>
+            </div>
+          </div>
+          <hr />
+          <div className="row d-flex">
+            <div className="col-6">
+              <iframe
+                src="https://greenclassifier.streamlit.app/"
+                style={{ border: "1px solid black" }}
+                width="90%"
+                height="300px"
+              />
+            </div>
+            <div className="col-6">
+              <h3>Green Classifier Model</h3>
+              <p>
+                {" "}
+                Vegetable / Fruit Classification model.
+                <li>
+                  Developed a Deep Learning model using TensorFlow and Keras to
+                  classify images of fruits/vegetables.
+                </li>
+                <li>
+                  Deployed on Streamlit with an intuitive UI, achieving 90%
+                  classification accuracy.
+                </li>
+                <a href="https://greenclassifier.streamlit.app/">Link</a>
+              </p>
+            </div>
+          </div>
+          <hr />
+          <div className="row d-flex">
+            <div className="col-6">
+              <iframe
+                src="https://prateeksri1110.github.io/Teahub/"
+                style={{ border: "1px solid black" }}
+                width="90%"
+                height="300px"
+              />
+            </div>
+            <div className="col-6">
+              <h3>TeaHub</h3>
+              <p>
+                {" "}
+                Tea ordering Web Portal
+                <li>
+                  Developed this web application with the help of tailwind CSS.
+                </li>
+                <li>Deployed on Github Pages.</li>
+                <a href="https://prateeksri1110.github.io/Teahub/">Link</a>
+              </p>
+            </div>
+          </div>
+        </div>
+        <br />
+        <br />
+        <h4 className="text-center">
+          Check Out my <a href="https://github.com/prateekSri1110">Github</a>{" "}
+          for more Porjects.
+        </h4>
+        <br />
+        <hr />
+      </section>
+    </header>
+  );
 }
--- src/component/skills.jsx
+++ src/component/skills.jsx
@@ -1,46 +1,119 @@
-import java from '../pngs/java.png';
-import cpp from '../pngs/cpp.png';
-import css3 from '../pngs/css3.png';
-import github from '../pngs/github.png';
-import mysql from '../pngs/mysql.png';
-import react from '../pngs/react.png';
-import html from '../pngs/html.png';
-import js from '../pngs/js.png';
-import springboot from '../pngs/spring-boot.png';
-import bootstrap from '../pngs/bootstrap.png';
-import python from '../pngs/python.png';
-import mongo from '../pngs/mongo.png';
-
+import java from "../pngs/java.png";
+import cpp from "../pngs/cpp.png";
+import css3 from "../pngs/css3.png";
+import github from "../pngs/github.png";
+import mysql from "../pngs/mysql.png";
+import react from "../pngs/react.png";
+import html from "../pngs/html.png";
+import js from "../pngs/js.png";
+import vue from "../pngs/vue.png";
+import springboot from "../pngs/spring-boot.png";
+import bootstrap from "../pngs/bootstrap.png";
+import python from "../pngs/python.png";
+import mongo from "../pngs/mongo.png";
 
 export default function Skills() {
-    return (
-        <div id="skill">
-            <br /><br /><hr />
+  return (
+    <div id="skill">
+      <br />
+      <br />
+      <hr />
 
-            <div className="container mt-5">
-                <div className="row justify-content-center">
-                    <h3 className="text-center mside">SKILLS</h3>
-                    <br /><br />
-                    <div className='col-4 p-3 text-center'>
-                        <img style={{ width: "100px", padding: "15px" }} src={java} alt="java" />
-                        <img style={{ width: "100px", padding: "15px" }} src={cpp} alt="cpp" />
-                        <img style={{ width: "100px", padding: "15px" }} src={python} alt="python" />
-                        <img style={{ width: "100px", padding: "15px" }} src={mysql} alt="mysql" />
-                    </div>
-                    <div className='col-4 p-3 text-center'>
-                        <img style={{ width: "100px", padding: "15px" }} src={html} alt="html" />
-                        <img style={{ width: "100px", padding: "15px" }} src={js} alt="js" />
-                        <img style={{ width: "100px", padding: "15px" }} src={css3} alt="css3" />
-                        <img style={{ width: "100px", padding: "15px" }} src={bootstrap} alt="bootstrap" />
-                    </div>
-                    <div className='col-4 p-3 text-center'>
-                        <img style={{ width: "100px", padding: "15px" }} src={react} alt="React" />
-                        <img style={{ width: "120px", padding: "15px" }} src={mongo} alt="mongoDB" />
-                        <img style={{ width: "120px", padding: "15px" }} src={springboot} alt="springboot" />
-                        <img style={{ width: "100px", padding: "15px" }} src={github} alt="github" />
-                    </div>
-                </div>
-            </div>
+      <div className="container mt-5">
+        <div className="row justify-content-center">
+          <h3 className="text-center mside">SKILLS</h3>
+          <br />
+          <br />
+          <div className="col-4 p-3 text-center">
+            <img
+              style={{ width: "100px", padding: "15px" }}
+              src={java}
+              alt="java"
+            />
+            <img
+              style={{ width: "100px", padding: "15px" }}
+              src={cpp}
+              alt="cpp"
+            />
+            <img
+              style={{ width: "100px", padding: "15px" }}
+              src={python}
+              alt="python"
+            />
+            <img
+              style={{ width: "100px", padding: "15px" }}
+              src={mysql}
+              alt="mysql"
+            />
+          </div>
+          <div className="col-4 p-3 text-center">
+            <img
+              style={{ width: "100px", padding: "15px" }}
+              src={html}
+              alt="html"
+            />
+            <img
+              style={{ width: "100px", padding: "15px" }}
+              src={js}
+              alt="js"
+            />
+            <img
+              style={{ width: "100px", padding: "15px" }}
+              src={css3}
+              alt="css3"
+            />
+            <img
+              style={{ width: "100px", padding: "15px" }}
+              src={bootstrap}
+              alt="bootstrap"
+            />
+          </div>
+          <div className="col-4 p-3 text-center">
+            <img
+              style={{ width: "100px", padding: "15px" }}
+              src={react}
+              alt="React"
+            />
+            <img
+              style={{ width: "120px", padding: "15px" }}
+              src={mongo}
+              alt="mongoDB"
+            />
+            <img
+              style={{ width: "120px", padding: "15px" }}
+              src={springboot}
+              alt="springboot"
+            />
+            <img
+              style={{ width: "100px", padding: "15px" }}
+              src={github}
+              alt="github"
+            />
+          </div>
+          <div className="col-4 p-3 text-center">
+            <img
+              style={{ width: "100px", padding: "15px" }}
+              src={vue}
+              alt="Vue"
+            />
+            {/* <img
+              style={{ width: "120px", padding: "15px" }}
+              src={mongo}
+              alt="mongoDB"
+            />
+            <img
+              style={{ width: "120px", padding: "15px" }}
+              src={springboot}
+              alt="springboot"
+            />
+            <img
+              style={{ width: "100px", padding: "15px" }}
+              src={github}
+              alt="github"
+            /> */}
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
